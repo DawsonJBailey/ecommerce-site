c97cf110eeea0fb717ad5a27c0de7c62001ee415
dxb120801@darden.com <dxb120801@darden.com>
2023-02-02 14:46:11 -0500

    added redux persist

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import { Provider } from "react-redux";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <BrowserRouter>
         {/* <UserProvider> */}
           {/* <CategoriesProvider> */}
@@ -23,6 +25,7 @@ root.render(
           {/* </CategoriesProvider> */}
         {/* </UserProvider> */}
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
