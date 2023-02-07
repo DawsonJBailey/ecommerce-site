098e43f047cf45b87022a13a4809d629723758a9
dxb120801@darden.com <dxb120801@darden.com>
2023-02-07 14:32:12 -0500

    added stripe and netlify cli

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,16 +13,43 @@ const PaymentForm = () => {
         if(!stripe || !elements) {
             return;
         }
-        
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({amount: 10000})
+        }).then(res => res.json())
+
+        console.log(response);
+        const clientSecret = response.paymentIntent.client_secret;
+
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Dawson Bailey'
+                }
+            }
+        })
+        if(paymentResult.error) {
+            alert(paymentResult.error);
+        }
+        else {
+            if(paymentResult.paymentIntent.status === 'succeeded') {
+                alert('payment successful')
+            }
+        }
     }
 
     return(
         <div className={'payment-form-container'}>
-            <div className={'form-container'}>
+            <form className={'form-container'} onSubmit={paymentHandler}>
             <h2>Credit Card Payment: </h2>
             <CardElement />
             <Button buttonType={'inverted'}>Pay now</Button>
-            </div>
+            </form>
         </div>
     )
 }
